ea75b0234b8005a883d26ae95cff7b9bfc2de23e
Aurélien Bompard <aurelien@bompard.org>
2010-01-07 15:21:50 +0100

    Adapt xhtml2odt.py to the XSL-based style adding
    114 less lines, yay !

--- xhtml2odt.py
+++ xhtml2odt.py
@@ -100,10 +100,6 @@ class ODTFile(object):
             "styles": "",
         }
         self.tmpdir = tempfile.mkdtemp(prefix="xhtml2odt-")
-        self.styles = {}
-        self.autostyles = {}
-        self.style_name_re = re.compile('style:name="([^"]+)"') 
-        self.fonts = {}
 
     def open(self):
         self.zfile = zipfile.ZipFile(self.options.template, "r")
@@ -274,60 +270,19 @@ class ODTFile(object):
                 '</office:text>',
                 content + '</office:text>')
 
-    def import_style(self, style, is_mainstyle=False):
-        style_name_mo = self.style_name_re.search(style)
-        name = style_name_mo.group(1)
-        if name in self.styles:
-            return # already added
-        if self.xml["content"].count('style:name="%s"' % name) > 0 or \
-           self.xml["styles"].count('style:name="%s"' % name) > 0:
-            return # already present in the template
-        if is_mainstyle:
-            self.styles[name] = style
-        else:
-            self.autostyles[name] = style
-
-    def import_font(self, font):
-        style_name_mo = self.style_name_re.search(font)
-        name = style_name_mo.group(1)
-        if name in self.fonts:
-            return # already added
-        if self.xml["styles"].count('<style:font-face style:name="%s"'
-                                    % name) > 0 or \
-            self.xml["content"].count('<style:font-face style:name="%s"'
-                                    % name) > 0:
-            return # already present in the template
-        self.fonts[name] = font
-
     def add_styles(self):
-        odtstyle = ODTStyle(self.template_dirs)
-        odtstyle.add_styles(self.xml["content"], self.import_style,
-                            self.import_font)
+        xsl_dir = os.path.join(INSTALL_PATH, 'xsl')
+        xslt_doc = etree.parse(os.path.join(xsl_dir, "styles.xsl"))
+        transform = etree.XSLT(xslt_doc)
+        contentxml = etree.fromstring(self.xml["content"])
+        stylesxml = etree.fromstring(self.xml["styles"])
+        params = {}
+        if self.options.verbose:
+            params["debug"] = "1"
+        self.xml["content"] = str(transform(contentxml, **params))
+        self.xml["styles"] = str(transform(stylesxml, **params))
 
     def compile(self):
-        # autostyles
-        if self.autostyles:
-            autostyles = "\n".join(self.autostyles.values())
-            for xmlfile in ["content", "styles"]:
-                if self.xml[xmlfile].count("<office:automatic-styles/>") > 0:
-                    self.xml[xmlfile] = self.xml[xmlfile].replace(
-                        "<office:automatic-styles/>",
-                        "<office:automatic-styles>%s" % autostyles + \
-                        "</office:automatic-styles>")
-                else:
-                    self.xml[xmlfile] = self.xml[xmlfile].replace(
-                        "</office:automatic-styles>",
-                        "%s</office:automatic-styles>" % autostyles)
-        if self.styles:
-            styles = "\n".join(self.styles.values())
-            self.xml["styles"] = self.xml["styles"].replace(
-                "</office:styles>", "%s</office:styles>" % styles)
-        if self.fonts:
-            fonts = "\n".join(self.fonts.values())
-            for xmlfile in ["content", "styles"]:
-                self.xml[xmlfile] = self.xml[xmlfile].replace(
-                    "</office:font-face-decls>",
-                    "%s</office:font-face-decls>" % fonts)
         # Store the new content
         for xmlfile in self.xml:
             xmlf = open(os.path.join(self.tmpdir, "%s.xml" % xmlfile), "w")
@@ -353,75 +308,6 @@ class ODTFile(object):
         shutil.rmtree(self.tmpdir)
         if not output:
             return document.getvalue()
-
-
-class ODTStyle(object):
-    """
-    This class contains the ODT style library
-    """
-    # pylint#: disable-msg=C0103
-
-    def __init__(self, template_dirs):
-        self.template_dirs = template_dirs
-        self.style_name_re = re.compile('style:name="([^"]+)"') 
-        self.need_font_re = re.compile('font-name="([^"]+)"')
-
-    def _build_style_lib(self):
-        """build a library of available styles"""
-        template_files = self._build_templates_list()
-        style_lib = {}
-        for style_file in template_files.values():
-            style_tpl = open(style_file)
-            style_xml = style_tpl.read()
-            style_tpl.close()
-            style = self._build_style(style_xml)
-            if not style:
-                continue
-            style_lib[style["name"]] = style
-        return style_lib
-
-    def _build_templates_list(self):
-        """select the preferred template (used-defined or default)"""
-        template_files = {}
-        for template_dir in self.template_dirs:
-            for style_file in glob(os.path.join(template_dir, "*.txt")):
-                basename = os.path.basename(style_file)
-                if basename not in template_files:
-                    template_files[basename] = style_file
-        return template_files
-
-    def _build_style(self, style_xml):
-        style_name_mo = self.style_name_re.search(style_xml)
-        if not style_name_mo:
-            return False
-        style = {
-            "name": style_name_mo.group(1),
-            "xml": style_xml,
-        }
-        is_mainstyle = (style_xml.count("style:display-name=") > 0)
-        style["mainstyle"] = is_mainstyle
-        need_font = self.need_font_re.search(style_xml)
-        if need_font:
-            style["need_font"] = need_font.group(1)
-        return style
-
-
-    def add_styles(self, content_xml, import_style_callback,
-                                      import_font_callback):
-        """
-        Add the missing styles using callbacks
-        """
-        style_lib = self._build_style_lib()
-        for stylename in style_lib:
-            if content_xml.count('style-name="%s"' % stylename) == 0:
-                continue # style is not used
-            style_xml = style_lib[stylename]["xml"]
-            is_mainstyle = style_lib[stylename]["mainstyle"]
-            import_style_callback(style_xml, is_mainstyle)
-            if "need_font" in style_lib[stylename]:
-                font_name = style_lib[stylename]["need_font"]
-                font_xml = style_lib[font_name]["xml"]
-                import_font_callback(font_xml)
 
 
 def log(msg, verbose=False):
