5be226f2ac8f3c7a16d8e11f96cb4753b7e4f9a6
Aurélien Bompard <aurelien@bompard.org>
2010-03-19 07:35:58 +0100

    Add full functionnal test

---
+++ tests/test_full.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import re
+import unittest
+from lxml import etree
+from . import xhtml2odt
+
+full_test_cleanup = re.compile('draw:name="imageobject-id\d+"')
+
+class Full(unittest.TestCase):
+
+    def _run_full_test(self, testid):
+        tests_data = os.path.join(os.path.dirname(__file__), "data")
+        html_file = open(os.path.join(tests_data, "%s.xhtml" % testid))
+        html = html_file.read()
+        html_file.close()
+        xml_file = open(os.path.join(tests_data, "%s.xml" % testid))
+        xml = xml_file.read()
+        xml_file.close()
+        odt = str(xhtml2odt(html))
+        xml = full_test_cleanup.sub("", xml)
+        odt = full_test_cleanup.sub("", odt)
+        #xml_file = open(os.path.join(tests_data, "%s.xml.orig" % testid), "w")
+        #xml_file.write(odt)
+        #xml_file.close()
+        assert odt == xml
+
+    def test_full_1(self):
+        return self._run_full_test(1)
+
+if __name__ == '__main__':
+    unittest.main()
