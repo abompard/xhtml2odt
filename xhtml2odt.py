feebfe24bc169c2c968c567ef1a6e6656400e4ce
Aurélien Bompard <aurelien@bompard.org>
2010-04-03 23:57:41 +0200

    Add extensive documentation

--- xhtml2odt.py
+++ xhtml2odt.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
 """
-    xhtml2odt - XHTML to ODT XML transformation.
-    Copyright (C) 2009-2010 Aurelien Bompard
+xhtml2odt - XHTML to ODT XML transformation
+===========================================
+
+Copyright (C) 2009-2010 Aurelien Bompard
 
 This script can convert a wiki page to the OpenDocument Text (ODT) format,
 standardized as ISO/IEC 26300:2006, and the native format of office suites such
@@ -11,8 +13,68 @@ as OpenOffice.org, KOffice, and others.
 It uses a template ODT file which will be filled with the converted content of
 the exported Wiki page.
 
-Based on the work on docbook2odt, by Roman Fordinal
-http://open.comsultia.com/docbook2odf/
+Based on the work on docbook2odt_, by Roman Fordinal
+
+.. _docbook2odt: http://open.comsultia.com/docbook2odf/
+
+
+Usage
+-----
+
+Call the script with the :option:`--help` option to see all the available
+options.  The main options are:
+
+.. cmdoption:: -i <file>, --input <file>
+
+   The HTML file to read from.
+
+.. cmdoption:: -o <file>, --output <file>
+
+   The ODT file to export to (will be overwritten if already present).
+
+.. cmdoption:: -t <file>, --template <file>
+
+   The ODT file to use as a template (must be readable).
+
+.. cmdoption:: -v
+
+   Be verbose (enables logging)
+
+
+The full help message is::
+
+    Usage: xhtml2odt.py [options] -i input -o output -t template.odt
+    
+    Options:
+      -h, --help            show this help message and exit
+      -i FILE, --input=FILE
+                            Read the html from this file
+      -o FILE, --output=FILE
+                            Location of the output ODT file
+      -t FILE, --template=FILE
+                            Location of the template ODT file
+      -u URL, --url=URL     Use this URL for relative links
+      -v, --verbose         Show what's going on
+      --html-id=ID          Only export from the element with this ID
+      --replace=KEYWORD     Keyword to replace in the ODT template (default is
+                            ODT-INSERT)
+      --cut-start=KEYWORD   Keyword to start cutting text from the ODT template
+                            (default is ODT-CUT-START)
+      --cut-stop=KEYWORD    Keyword to stop cutting text from the ODT template
+                            (default is ODT-CUT-STOP)
+      --top-header-level=LEVEL
+                            Level of highest header in the HTML (default is 1)
+      --img-default-width=WIDTH
+                            Default image width (default is 8cm)
+      --img-default-height=HEIGHT
+                            Default image height (default is 6cm)
+      --dpi=DPI             Screen resolution in Dots Per Inch (default is 96)
+      --no-network          Do not download remote images
+      --stylesdir=DIR       Override the style templates directory
+
+
+License
+-------
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
@@ -51,12 +113,27 @@ CHARSET = "utf-8"
 class ODTExportError(Exception): pass
 
 class HTMLFile(object):
+    """
+    This class contains the HTML document to convert to ODT. The HTML code
+    will be run through Tidy to ensure that is is valid and well-formed
+    XHTML.
+
+    :ivar options: An OptionParser-result object containing the options for
+        processing.
+    :type options: OptionsParser-result object
+    :ivar html: The HTML code.
+    :type html: ``str``
+    """
 
     def __init__(self, options):
         self.options = options
         self.html = ""
 
     def read(self):
+        """
+        Read the HTML file from :attr:`options`.input, run it through Tidy, and
+        filter using the selected ID (if applicable).
+        """
         in_file = open(self.options.input)
         self.html = in_file.read()
         in_file.close()
@@ -65,6 +142,9 @@ class HTMLFile(object):
             self.select_id()
 
     def cleanup(self):
+        """
+        Run the HTML code from the :attr:`html` instance variable through Tidy.
+        """
         tidy_options = dict(output_xhtml=1, add_xml_decl=1, indent=1,
                             tidy_mark=0, #input_encoding=str(self.charset),
                             output_encoding='utf8', doctype='auto',
@@ -77,6 +157,10 @@ class HTMLFile(object):
         self.html = re.sub('<pre([^>]*)>\n', '<pre\\1>', self.html)
 
     def select_id(self):
+        """
+        Replace the HTML content by an element in the content. The element
+        is selected by its HTML ID.
+        """
         html_tree = etree.fromstring(self.html)
         selected = html_tree.xpath("//*[@id='%s']" % self.options.htmlid)
         self.html = etree.tostring(selected[0], method="html")
@@ -100,6 +184,10 @@ class ODTFile(object):
         self.tmpdir = tempfile.mkdtemp(prefix="xhtml2odt-")
 
     def open(self):
+        """
+        Uncompress the template ODT file, and read the content.xml and
+        styles.xml files into memory.
+        """
         self.zfile = zipfile.ZipFile(self.options.template, "r")
         for name in self.zfile.namelist():
             fname = os.path.join(self.tmpdir, name)
@@ -116,11 +204,32 @@ class ODTFile(object):
             self.xml[xmlfile] = self.zfile.read("%s.xml" % xmlfile)
 
     def import_xhtml(self, xhtml):
+        """
+        Main function to run the conversion process:
+
+        * XHTML import
+        * conversion to ODT XML
+        * insertion into the ODT template
+        * adding of the missing styles
+
+        The next logical step is to use the :meth:`save` method.
+
+        :param xhtml: the XHTML content to import
+        :type  xhtml: str
+        """
         odt = self.xhtml_to_odt(xhtml)
         self.insert_content(odt)
         self.add_styles()
 
     def xhtml_to_odt(self, xhtml):
+        """
+        Converts the XHTML content into ODT.
+
+        :param xhtml: the XHTML content to import
+        :type  xhtml: str
+        :returns: the ODT XML from the conversion
+        :rtype: str
+        """
         xsl_dir = os.path.join(INSTALL_PATH, 'xsl')
         xslt_doc = etree.parse(os.path.join(xsl_dir, "xhtml2odt.xsl"))
         transform = etree.XSLT(xslt_doc)
@@ -143,6 +252,16 @@ class ODTFile(object):
         return str(odt).replace('<?xml version="1.0" encoding="utf-8"?>','')
 
     def handle_images(self, xhtml):
+        """
+        Handling of image tags in the XHTML. Local and remote images are
+        handled differently: see the :meth:`handle_local_img` and
+        :meth:`handle_remote_img` methods for details.
+
+        :param xhtml: the XHTML content to import
+        :type  xhtml: str
+        :returns: XHTML with normalized ``img`` tags
+        :rtype: str
+        """
         # Handle local images
         xhtml = re.sub('<img [^>]*src="([^"]+)"[^>]*>',
                       self.handle_local_img, xhtml)
@@ -154,6 +273,21 @@ class ODTFile(object):
         return xhtml
 
     def handle_local_img(self, img_mo):
+        """
+        Handling of local images. This method should be called as a callback on
+        each ``img`` tag.
+
+        Find the real path of the image file and use the :meth:`handle_img`
+        method to flag it for inclusion in the ODT file.
+
+        This implementation downloads the files that come from the same domain
+        as the XHTML document cames from, but server-based export plugins can
+        just retrieve it from the local disk, using either the
+        ``DOCUMENT_ROOT`` or any appropriate method (depending on the web
+        application you're writing an export plugin for).
+
+        :param img_mo: the match object from the `re.sub` callback
+        """
         log("handling local image: %s" % img_mo.group(1), self.options.verbose)
         src = img_mo.group(1)
         if src.count("://") and not src.startswith("file://"):
@@ -184,6 +318,12 @@ class ODTFile(object):
         return ret
 
     def handle_remote_img(self, img_mo):
+        """
+        Downloads remote images to a temporary file and flags them for
+        inclusion using the :meth:`handle_img` method.
+
+        :param img_mo: the match object from the `re.sub` callback
+        """
         log('handling remote image: %s' % img_mo.group(), self.options.verbose)
         src = img_mo.group(1)
         try:
@@ -196,7 +336,10 @@ class ODTFile(object):
 
     def download_img(self, src):
         """
-        Download the image to a temporary location
+        Downloads the given image to a temporary location.
+
+        :param src: the URL to download
+        :type  src: str
         """
         log('Downloading image: %s' % src, self.options.verbose)
         # TODO: proxy support
@@ -209,6 +352,16 @@ class ODTFile(object):
         return tmpfile
 
     def handle_img(self, full_tag, src, filename):
+        """
+        Imports an image into the ODT file.
+
+        :param full_tag: the full ``img`` tag in the original XHTML document
+        :type  full_tag: str
+        :param src: the ``src`` attribute of the ``img`` tag
+        :type  src: str
+        :param filename: the path to the image file on the local disk
+        :type  filename: str
+        """
         log('Importing image: %s' % filename, self.options.verbose)
         if not os.path.exists(filename):
             raise ODTExportError('Image "%s" is not readable or does not exist'
@@ -267,13 +420,20 @@ class ODTFile(object):
         return full_tag
 
     def handle_links(self, xhtml):
-        """Turn relative links into absolute links"""
+        """
+        Turn relative links into absolute links using the :meth:`handle_links`
+        method.
+        """
         # Handle local images
         xhtml = re.sub('<a [^>]*href="([^"]+)"',
                       self.handle_relative_links, xhtml)
         return xhtml
 
     def handle_relative_links(self, link_mo):
+        """
+        Do the actual conversion of links from relative to absolute. This
+        method is used as a callback by the :meth:`handle_links` method.
+        """
         href = link_mo.group(1)
         if href.startswith("file://") or not self.options.url:
             # There's nothing we can do here
@@ -286,6 +446,13 @@ class ODTFile(object):
         return link_mo.group().replace(href, newhref)
 
     def insert_content(self, content):
+        """
+        Insert ODT XML content into the ``content.xml`` file, replacing the
+        keywords if needed.
+
+        :param content: ODT XML content to insert
+        :type  content: str
+        """
         if self.options.replace_keyword and \
             self.xml["content"].count(self.options.replace_keyword) > 0:
             self.xml["content"] = re.sub(
@@ -308,6 +475,10 @@ class ODTFile(object):
                     "", self.xml["content"])
 
     def add_styles(self):
+        """
+        Scans the ODT XML for used styles that would not be already included in
+        the ODT template, and adds those missing styles.
+        """
         xsl_dir = os.path.join(INSTALL_PATH, 'xsl')
         xslt_doc = etree.parse(os.path.join(xsl_dir, "styles.xsl"))
         transform = etree.XSLT(xslt_doc)
@@ -320,6 +491,9 @@ class ODTFile(object):
         self.xml["styles"] = str(transform(stylesxml, **params))
 
     def compile(self):
+        """
+        Writes the in-memory ODT XML content and styles to the disk
+        """
         # Store the new content
         for xmlfile in self.xml:
             xmlf = open(os.path.join(self.tmpdir, "%s.xml" % xmlfile), "w")
@@ -327,15 +501,31 @@ class ODTFile(object):
             xmlf.close()
 
     def _build_zip(self, document):
+        """
+        Zips the working directory into a :class:`zipfile.ZipFile` object
+
+        :param document: where the :class:`ZipFile` will be stored
+        :type  document: str or file-like object
+        """
         newzf = zipfile.ZipFile(document, "w", zipfile.ZIP_DEFLATED)
         for root, dirs, files in os.walk(self.tmpdir):
-            for file in files:
-                realpath = os.path.join(root, file)
-                internalpath = os.path.join(root.replace(self.tmpdir, ""), file)
+            for cur_file in files:
+                realpath = os.path.join(root, cur_file)
+                internalpath = os.path.join(root.replace(self.tmpdir, ""), cur_file)
                 newzf.write(realpath, internalpath)
         newzf.close()
 
     def save(self, output=None):
+        """
+        General method to save the in-memory content to an ODT file on the disk.
+
+        If :attr:`output` is ``None``, the document is returned.
+
+        :param output: where the document should be saved, see the :option:`-o`
+            option.
+        :type  output: str or file-like object or ``None``
+        :returns: if output is None: the ODT document ; or else ``None``.
+        """
         self.compile()
         if output:
             document = output
@@ -348,10 +538,17 @@ class ODTFile(object):
 
 
 def log(msg, verbose=False):
+    """
+    Simple method to log if we're in verbose mode (with the :option:`-v`
+    option).
+    """
     if verbose:
         sys.stderr.write(msg+"\n")
 
 def get_options():
+    """
+    Parses the command-line options.
+    """
     usage = "usage: %prog [options] -i input -o output -t template.odt"
     parser = OptionParser(usage=usage)
     parser.add_option("-i", "--input", dest="input", metavar="FILE",
@@ -413,6 +610,9 @@ def get_options():
     return options
 
 def main():
+    """
+    Main function, called when the script is invoked on the command line.
+    """
     options = get_options()
     htmlfile = HTMLFile(options)
     htmlfile.read()
