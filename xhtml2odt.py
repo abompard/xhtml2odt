aeca2eb3a098caec6105d3c5e1e2ec134853c9d7
Aurélien Bompard <aurelien@bompard.org>
2010-04-08 09:18:07 +0200

    Small documentation fix

--- xhtml2odt.py
+++ xhtml2odt.py
@@ -85,6 +85,9 @@ This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
+
+Code
+----
 """
 
 import tempfile
